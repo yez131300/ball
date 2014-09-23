c68dcb85078d79b8e334b3eae270f53199aa5b3c
yez131300 <490767881@qq.com>
2014-09-23 23:41:52 +0800

    22222222

---
+++ roll ball/src/resource.js
@@ -0,0 +1,18 @@
+var s_HelloWorld = "res/HelloWorld.png";
+var s_bg = "res/bg.jpg";
+
+var g_ressources = [
+    //image
+    {src:s_HelloWorld},
+    {src:s_bg}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
