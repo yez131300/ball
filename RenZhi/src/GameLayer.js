07dd0fd63022820f738ac4fd90019227f96964af
yez131300 <490767881@qq.com>
2014-11-14 23:32:32 +0800

    change to use menuItemSprite and create random number between 0 and 1

--- RenZhi/src/GameLayer.js
+++ RenZhi/src/GameLayer.js
@@ -1,18 +1,38 @@
 var GameLayer = cc.Layer.extend({
 	oneSprite : null,
 	zeroSprite : null,
+	currentInputValue:null,
 	onEnter:function(){
 		this._super();
 		// init one sprite
-		this.oneSprite = new InputSprite();
-		this.oneSprite.initData(this,res.one_png,1);
-		this.oneSprite.setPosition(200, 100);
-		this.addChild(this.oneSprite)
+
+		var oneItemPlay = cc.MenuItemSprite.create(cc.Sprite.create(res.one_png),
+				cc.Sprite.create(res.one_png),
+				this.inputOne, this);
+		var oneMenu = cc.Menu.create(oneItemPlay);
+		oneMenu.setPosition(200, 100);
+		this.addChild(oneMenu);
 		
 		// init zero sprite
-		this.zeroSprite = new InputSprite();
-		this.zeroSprite.initData(this, res.zero_png, 0);
-		this.zeroSprite.setPosition(330, 100);
-		this.addChild(this.zeroSprite);
+		var zeroItemPlay = cc.MenuItemSprite.create(cc.Sprite.create(res.zero_png),
+				cc.Sprite.create(res.zero_png),
+				this.inputZero,this);
+		var zeroMenu = cc.Menu.create(zeroItemPlay);
+		zeroMenu.setPosition(330, 100);
+		this.addChild(zeroMenu);
+		
+		this.schedule(this.update,0);
+	},
+	inputOne:function(){
+		this.currentInputValue =1;
+	},
+	inputZero:function(){
+		this.currentInputValue =0;
+	},
+	createRandom:function(){
+		return Math.round(Math.random());
+	},
+	update:function(dt){
+		cc.log("----------------------------"+this.createRandom());
 	}
 });
