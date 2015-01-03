695dd80143238ffe3926a59b86861c4af4a6afa0
yez131300 <490767881@qq.com>
2015-01-03 23:06:04 +0800

    V2 first commit

--- RenZhi_V2/src/GameLayer.js
+++ RenZhi_V2/src/GameLayer.js
@@ -1,8 +1,7 @@
 var GameLayer = cc.Layer.extend({
 	oneSprite : null,
 	zeroSprite : null,
-	currentInputValue:null,
-	currentValue:null,
+	currentValue:[],
 	numberList : [],
 	size:null,
 	numberLabel:null,
@@ -10,10 +9,7 @@ var GameLayer = cc.Layer.extend({
 	updateLatency:60,
 	gameStatus:0,   // 0--> normal  1-->over
 	renZhiList:[],
-	finishAct:true,
-	moveTime : 1,
-	fadeOutTime:0.5,
-	scaleTime:1,
+	finishAct:false,
 	point:0,
 	onEnter:function(){
 		this._super();
@@ -34,126 +30,105 @@ var GameLayer = cc.Layer.extend({
 		zeroMenu.setPosition(330, 80);
 		this.addChild(zeroMenu);
 		
-		this.initNumberList();
+		
+		this.size = 4;
+	
 		
 		//init the number label
-//		this.numberLabel = cc.LabelTTF.create(this.showNumberList(), "Helvetica", 20);
-//		this.numberLabel.setColor(cc.color(0,0,0));//black color
-//		this.numberLabel.setPosition(cc.winSize.width/2, cc.winSize.height - 50);
-//		this.addChild(this.numberLabel);
+		this.numberLabel = cc.LabelTTF.create(this.showCurrentValueList(), "Helvetica", 20);
+		this.numberLabel.setColor(cc.color(0,0,0));//black color
+		this.numberLabel.setPosition(cc.winSize.width/2, cc.winSize.height - 50);
+		this.addChild(this.numberLabel);
 		
 		
-		this.initRenzhi();
+		
 
 		this.schedule(this.update);
-		this.schedule(this.updateRenZhi);
+		
 	},
 	update:function(dt){
 		if(this.gameStatus == 1) return;
-		if(this.finishAct){
-			this.finishAct =false; // 重置标志
+		if(this.renZhiList.length == 0){
+			this.updateRenZhi();
+			this.finishAct = false;
+		}
+		for(var i = 0;i<this.renZhiList.length;i++){			
 			var act1 = cc.Sequence.create(
-					cc.MoveTo.create(this.moveTime, cc.p(55, 220)),
-					cc.CallFunc.create(this.finishMove,this));
-			var act2 =cc.Sequence.create(
-					cc.scaleBy(this.scaleTime,1.2),
-					cc.FadeOut.create(this.fadeOutTime),
-					cc.CallFunc.create(this.finishAction,this));
-			var act = cc.Sequence.create(act1,act2);
-			this.renZhiList[0].runAction(act);
+					cc.moveBy(0.0001,cc.p(-1, 0)),
+					cc.CallFunc.create(this.occurRenZhi,this));
+			
+			this.renZhiList[i].runAction(act1);
 			
 		}
-	},
-	finishAction:function(){
-//		this.renZhiList.splice(0, 1);
-//		this.updateRenZhi();
-		this.finishAct =true;
-		if(this.currentInputValue == this.currentValue){
-			this.point ++;
-			this.currentInputValue = null; // reset the current input for next input
+		if(this.renZhiList[0].x <= 20){
+			this.finishAct = true;
+			this.removeRenzhi();
+			if(this.renZhiList.length == 0){
+				if(this.checkResult() == true){
+					this.point ++;
+				}else{
+					this.gameStatus =1;
+				}
+				
+			}
 			
-//			// increase the speed
-			this.moveTime -= 0.005*(this.point/5);
-			if(this.moveTime <=0.3){
-				this.moveTime =0.3;
-			}
-			this.fadeOutTime -= 0.002*(this.point/5);
-			if(this.fadeOutTime <=0.35){
-				this.fadeOutTime =0.35;
-			}
-			this.scaleTime -= 0.002*(this.point/5);
-			if(this.scaleTime <=0.35){
-				this.scaleTime =0.35;
-			}
-			cc.log(this.moveTime + "---"+this.fadeOutTime+"----"+this.scaleTime)
-		}else{
-			this.gameStatus = 1 //game over
 		}
+		
 	},
-	finishMove:function(){
-		this.currentValue = this.renZhiList[0].value 
-		this.renZhiList.splice(0, 1);	
+	removeRenzhi:function(){
+		this.removeChild(this.renZhiList[0], true);
+		this.renZhiList.splice(0, 1);
 	},
 	inputOne:function(){
-		this.currentInputValue =1;
+		this.currentValue.push(1);
+		this.numberLabel.setString(this.showCurrentValueList());
 	},
 	inputZero:function(){
-		this.currentInputValue =0;
+		this.currentValue.push(0);
+		this.numberLabel.setString(this.showCurrentValueList());
 	},
 	createRandom:function(){
 		return Math.round(Math.random());
 	},
-	initNumberList : function(){
-		this.size = 4;
-		for(var i = 0;i<this.size;i++){
-			this.numberList[i]=this.createRandom();
-		}
-	},
 	updateRenZhi:function(){
 		if(this.gameStatus == 1) return;
-		if(this.renZhiList.length<this.size){
-		this.numberList[this.size] = this.createRandom();
-		var renZhi = new RenZhiSprite();
-		if(this.numberList[this.size] == 1){
-			renZhi.initData(res.renOne_png,1);
-		}else{
-			renZhi.initData(res.renZero_png,0);
-		}
-		renZhi.setScale(40/100);
-		renZhi.setPosition(cc.winSize.width-45, 220)
-		for(var i = 0;i<this.size-1;i++){
-			this.renZhiList[i].runAction(
-					cc.moveTo(0.1,cc.p(cc.winSize.width-45*(this.size-i), 220)));
-		}
-		
-		this.addChild(renZhi);
-		this.renZhiList[this.size-1]=renZhi;
-		for(var i = 0;i<this.size;i++){
-			this.numberList[i] = this.numberList[i+1];
-			
-		}
-		}
+			if(this.renZhiList.length<this.size){
+				var num = this.createRandom();
+				this.numberList.push(num);
+				var renZhi = new RenZhiSprite();
+				if(num == 1){
+					renZhi.initData(res.renOne_png,1);
+				}else{
+					renZhi.initData(res.renZero_png,0);
+				}
+				renZhi.setScale(40/100);
+				renZhi.setPosition(cc.winSize.width-20, 220)
+				this.addChild(renZhi);
+				this.renZhiList.push(renZhi);
+				
+			}
 	},
-	showNumberList:function(){
+	showCurrentValueList:function(){
 		var result = "";
-		for(var i = 0;i<this.size;i++){
-			result+=this.numberList[i];
+		for(var i = 0;i<this.currentValue.length;i++){
+			result+=this.currentValue[i];
 		}
 		return result;
 	},
-	initRenzhi:function(){
-		for(var i = 0;i<this.size;i++){
-			var renZhi = new RenZhiSprite();
-			if(this.numberList[i] == 1){
-				renZhi.initData(res.renOne_png,1);
-			}else{
-				renZhi.initData(res.renZero_png,0);
-			}
-			renZhi.setScale(40/100);
-			renZhi.setPosition(cc.winSize.width-45*(this.size-i), 220);
-			this.renZhiList[i]=renZhi;
-			this.addChild(renZhi);
+	occurRenZhi : function(){
+		if(this.renZhiList.length > this.size) return
+		var lastRanzhi = this.renZhiList[this.renZhiList.length-1];
+		var lastX  = lastRanzhi.x;
+		if((cc.winSize.width-20) -lastX >= 40 && !this.finishAct){
+			this.updateRenZhi();
 		}
 	},
-	
+	checkResult : function () {
+		if(this.currentValue.length != this.size) return false;
+		for(var i = 0;i<this.size;i++){
+			if(this.numberList[i] != this.currentValue[i])
+				return false;
+		}
+		return true;
+	}
 });
